d412f688542014b42abf6882199732cef422ab03
Kirilo <kirillbaybula@gmail.com>
2020-07-16 13:48:36 +0300

    Add home page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from . import views
 
 urlpatterns = [
+    path('', views.home_render),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('forum/', include('Articles.url')),
