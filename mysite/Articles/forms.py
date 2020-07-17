0b65f580c044cdb36e9c4da129b8dc8ae5414af4
OmegaTymbJIep <57064990+OmegaTymbJIep@users.noreply.github.com>
2020-07-17 23:28:08 +0300

    front
    ololo

---
+++ mysite/Articles/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(
+        label='',
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': '',
+            'placeholder': 'Search',
+            'autocomplete': 'off'
+        }),
+        error_messages={}
+    )
+
+
+class FilterForm(forms.Form):
+    FILTER = (
+        ('from_new_to_old', 'From new to old'),
+        ('from_old_to_new', 'From old to new'),
+        ('most_popular', 'Most popular')
+    )
+    order_choices = forms.ChoiceField(
+        required=False,
+        widget=forms.Select(
+            attrs={
+                'class': '',
+
+            }
+        ),
+        choices=FILTER,
+    )
+
+    pub_date_from = forms.DateField(
+        required=False,
+        input_formats=['%d/%m/%Y'],
+        widget=forms.DateInput(
+            attrs={
+                'class': 'form-control datetimepicker-input',
+                'data-target': '#datetimepicker1'
+            }
+        )
+    )
+    pub_date_to = forms.DateField(
+        required=False,
+        input_formats=['%d/%m/%Y'],
+        widget=forms.DateInput(
+            attrs={
+                'class': 'form-control datetimepicker-input',
+                'data-target': '#datetimepicker1'
+            }
+        )
+    )
--- mysite/static/js/main.js
+++ mysite/static/js/main.js
@@ -5,8 +5,27 @@ $(document).ready(function(){
 
     $('.fullscreen').css('height', window.innerHeight);
     $('.halfscreen').css('height', window.innerHeight*0.6);
-    var sliderOne = slideShow('.slider', { isAutoplay: true });
-    var sliderOne = slideShow('.comment-article-slider', { isAutoplay: true });
+/*    var sliderOne = slideShow('.slider', { isAutoplay: true });
+    var sliderOne = slideShow('.comment-article-slider', { isAutoplay: true });*/
+
+    if(document.querySelector('.forum-unit-post-img')) {
+        document.querySelectorAll('.forum-unit-post-img').forEach(function(element) {
+            if (element.height >= element.width) {
+                element.style.height = '100%';
+                element.style.width = 'auto';
+            }
+            element.parentNode.childNodes[1].childNodes[1].src = element.src;
+        });
+    }
+    if(document.querySelector('.mouse-parallax-bg')) {
+        $(function(){
+            $('.mouse-parallax-bg').parallax({
+                mouseport: $('.forum-intro-section'),
+                decay: 0.9
+            });
+
+        });
+    }
 });
 
 window.addEventListener("resize", function() {
@@ -31,34 +50,36 @@ function hamburger() {
 }
 //    ======== END For hamburger anim, HEADER  2 ==============//
 
+if (document.querySelectorAll('.flying-inscription')) {
+    document.querySelectorAll('.flying-inscription').forEach(function(element) {
+        element.addEventListener('mouseover', function(event) {
 
-document.querySelectorAll('.flying-inscription').forEach(function(element) {
-    element.addEventListener('mouseover', function(event) {
+            var elements = [];
+            var text = event.target.parentNode.querySelector('.flying-inscription-text-box');
 
-        var elements = [];
-        var text = event.target.parentNode.querySelector('.flying-inscription-text-box');
+            document.querySelectorAll('.flying-inscription').forEach(el => {if(el != event.target) elements.push(el)});
 
-        document.querySelectorAll('.flying-inscription').forEach(el => {if(el != event.target) elements.push(el)});
+            event.target.style.opacity = 0;
+            var anim = setInterval(function() {
+                if (Number(elements[0].style.opacity) >= 0.4) {
+                    clearInterval(anim);
+                }
 
-        event.target.style.opacity = 0;
-        var anim = setInterval(function() {
-            if (Number(elements[0].style.opacity) >= 0.4) {
-                clearInterval(anim);
-            }
+                elements.forEach(function(el) {
+                    el.style.opacity = Number(el.style.opacity) + 0.1;
+                    text.style.opacity = Number(text.style.opacity) + 0.2;
+                });
+            }, 20);
 
-            elements.forEach(function(el) {
-                el.style.opacity = Number(el.style.opacity) + 0.1;
-                text.style.opacity = Number(text.style.opacity) + 0.2;
+            element.addEventListener('mouseout', function(event) {
+                clearInterval(anim)
+                document.querySelectorAll('.flying-inscription').forEach(el => {el.style.opacity = 0});
+                text.style.opacity = 0;
             });
-        }, 20);
-
-        element.addEventListener('mouseout', function(event) {
-            clearInterval(anim)
-            document.querySelectorAll('.flying-inscription').forEach(el => {el.style.opacity = 0});
-            text.style.opacity = 0;
         });
     });
-});
+}
+
 
 /*
 function headerAnimation() {
