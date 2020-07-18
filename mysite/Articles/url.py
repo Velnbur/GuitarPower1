56e0fbabc92f7941b5796c242be8680f9cd8a6f9
Kirilo <kirillbaybula@gmail.com>
2020-07-18 15:54:57 +0300

    Add articles on forum page

--- mysite/Articles/url.py
+++ mysite/Articles/url.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -7,6 +5,3 @@ urlpatterns = [
     path('<int:num>', views.forum_render),
     path('articles<int:num>', views.article_render),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
