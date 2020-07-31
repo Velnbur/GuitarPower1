88318334e58877fabbc44f9c2fb9ce593f97b73f
velnbur <kirillbaybula@gmail.com>
2020-07-31 11:46:20 +0300

    Add reset password function

--- mysite/ProfileApp/url.py
+++ mysite/ProfileApp/url.py
@@ -2,14 +2,32 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path(r'', views.profile_render),
     path(r'login/', views.login_view),
     path(r'logout/', views.logout_view),
     path(r'register/', views.register_view),
-    path(r'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+
+    path(r'activate/<uidb64>/<token>/',
          views.activate, name='activate'),
+
+    path(r'password_reset/',
+         auth_views.PasswordResetView.as_view(template_name='authentication/password_reset.html'),
+         name='password_reset'),
+
+    path(r'password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='authentication/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path(r'reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='authentication/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path(r'reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='authentication/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
